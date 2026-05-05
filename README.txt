b27823f5900a3c39ac21740f4033369c0699490d
j84bird19 <j84bird19@gmail.com>
2026-05-05 06:21:15 -0500

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,1 +1,8 @@
-V13 Font Upgrade applied
+Bird Planner V14 Large Print
+
+Bigger font version based on user's reference image:
+- Larger body text
+- Larger tabs
+- Larger labels, buttons, inputs
+- Larger calendar day numbers and preview entries
+- Larger cards and tracker values
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const KEY='bird_planner_v13_fonts';
+const KEY='bird_planner_v14_large_print';
 const MONTHS=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 const FULL_MONTHS=['January','February','March','April','May','June','July','August','September','October','November','December'];
 const SECTIONS={
